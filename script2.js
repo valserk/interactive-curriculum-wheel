950ffb49e0ff50af784a06579dc75ac586759a0e
valserk <32035357+valserk@users.noreply.github.com>
2025-09-30 22:28:12 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -49,10 +49,10 @@ document.addEventListener('DOMContentLoaded', () => {
           textColor: 'white', labelSize: '16px' }, 
 
         // Circle 6: Pink - 7 segments (240 * 1.2 = 288)
-        { id: 6, radius: 288, color: COLORS[5], isStatic: false, rotation: 0, targetRotation: 0, offset: 0, label: null, segments: ["FETAC", "JCSP", "Leaving Cert", "ICT/EAL", "In-house Certification", "Primary Curriculum", "Junior Cert"], textColor: 'white', labelSize: '16px' }, 
+        { id: 6, radius: 288, color: COLORS[5], isStatic: false, rotation: 0, targetRotation: 0, offset: 0, label: null, segments: ["FETAC", "JCSP", "Leaving Cert", "ICT/EAL", "In-house Certification", "Primary Curriculum", "Junior Cert"], textColor: 'white', labelSize: '18px' }, 
 
         // Circle 7: Indigo - 7 segments (280 * 1.2 = 336)
-        { id: 7, radius: 336, color: COLORS[6], isStatic: false, rotation: 0, targetRotation: 0, offset: 0, label: null, segments: ["Diversity", "Collaboration/Partnership", "Progression", "Equality", "Continuity", "Flexibility", "Personalised Learning"], textColor: 'white', labelSize: '16px' }, 
+        { id: 7, radius: 336, color: COLORS[6], isStatic: false, rotation: 0, targetRotation: 0, offset: 0, label: null, segments: ["Diversity", "Collaboration/Partnership", "Progression", "Equality", "Continuity", "Flexibility", "Personalised Learning"], textColor: 'white', labelSize: '18px' }, 
     ];
 
     /**
